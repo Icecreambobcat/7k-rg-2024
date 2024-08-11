a6191ebe0c95145be356430cd17047eb5679d11d
Icecreambobcat <icekingrex123@gmail.com>
2024-08-12 08:11:41 +1000

    Progress save & upload

---
+++ src/App/App.py
@@ -0,0 +1,16 @@
+from __future__ import annotations # Required for forward references
+from typing import List, Dict, Any, Union, Optional, Tuple, Type # Required for forward references
+
+from abc import ABC, abstractmethod # Required for abstract classes
+
+from threading import Thread # Required for threading
+import math 
+import os
+import sys 
+
+import pygame as pg
+
+
+class App:
+    def __init__(self) -> None:
+        pass
