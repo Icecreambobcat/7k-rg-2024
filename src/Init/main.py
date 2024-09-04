9d6c705950d3ae4a297bd061ee454a49fce6704f
Icecreambobcat <icekingrex123@gmail.com>
2024-09-04 11:44:49 +1000

    Removed unnecessary code

--- src/Init/main.py
+++ src/Init/main.py
@@ -1,8 +1,4 @@
 from App.App import App
-from States.Menu import Menu
-from States.Game import Game
-from States.LevelSelect import LevelSelect
-from States.Results import Results
 
 def main() -> None:
     Game = App()
