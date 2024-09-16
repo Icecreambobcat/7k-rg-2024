be0122d4cce84367890d3ecf5433cbabc37709d9
Icecreambobcat <icekingrex123@gmail.com>
2024-09-16 11:55:10 +1000

    Added frameworks
    Added some things for future use
    Highly likely to implement launch flags
    IMPLEMENT AFTER DEFINING LAUNCH STATES

--- src/Init/main.py
+++ src/Init/main.py
@@ -1,10 +1,13 @@
 from ..App.App import App
-
+from sys import exit
+import argparse as ap
 
 def main() -> None:
     Game = App()
     Game.run()
-
+    # potentially use launch flags/args IE:
+    # parser = ap.ArgumentParser()
+    # args = parser.parse_args()
 
 if __name__ == "__main__":
     main()
