13e833a4fba9c5e4cfc023584c2f96ca8f71be12
Icecreambobcat <icekingrex123@gmail.com>
2024-09-16 13:53:38 +1000

    Commented in future code
    Uncomment when fully implemented

--- src/Init/main.py
+++ src/Init/main.py
@@ -1,14 +1,44 @@
 from ..App.App import App
-from sys import exit
-import argparse as ap
+# from ..App.Conf import Conf
+# # from sys import exit
+# from argparse import ArgumentParser
+# import os.path as path
+# import subprocess
 
 
 def main() -> None:
+    # parser = ArgumentParser()
+    # group = parser.add_mutually_exclusive_group()
+    #
+    # group.add_argument(
+    #     "--version",
+    #     action="version",
+    #     version=f"{Conf.VERSION}",
+    #     help="Returns the version of the program",
+    # )
+    # group.add_argument(
+    #     "-l",
+    #     "--log",
+    #     action="store_true",
+    #     help="Logs events to a file",
+    # )
+    # group.add_argument(
+    #     "-c",
+    #     "--clean",
+    #     action="store_true",
+    #     help="Cleans extra files",
+    # )
+    #
+    # args = parser.parse_args()
+    #
+    # if args.log:
+    #     pass
+    #
+    # if args.clean:
+    #     pass
+
     Game = App()
     Game.run()
-    # potentially use launch flags/args IE:
-    # parser = ap.ArgumentParser()
-    # args = parser.parse_args()
 
 
 if __name__ == "__main__":
