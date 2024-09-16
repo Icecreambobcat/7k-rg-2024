8af06726fd184078e9191b188814d35b77eefb5a
Icecreambobcat <icekingrex123@gmail.com>
2024-09-16 11:53:57 +1000

    Conf docs
    Added some structuring for how the conf class should behave & be used
    Need to implement later

--- src/App/Conf.py
+++ src/App/Conf.py
@@ -2,7 +2,21 @@ import pickle
 
 
 class Conf:
+    """
+    The conf class is used to handle fixed and custom variables that define the application
+    Fixed configs are typically used for debugging and compatability
+    If there is no set of config files then the default will be used and saved
+    Otherwise the custom configs will be loaded memory and used
+    """
+
+    WINDOW = {"PLACEHOLDER": "PLACEHOLDER"}
+
+    KEYS = {"PLACEHOLDER": "PLACEHOLDER"}
+
+    DEFAULTS = {"PLACEHOLDER": "PLACEHOLDER"}
+
     def __init__(self):
+        # call this from somewhere else if there is no loadconf
         pass
 
 
