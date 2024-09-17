845087866b98440c94d10f418b2fe968e72c4847
Icecreambobcat <icekingrex123@gmail.com>
2024-09-17 10:17:16 +1000

    Window size implementation
    Attempts to use pyautogui to create a window size dataset
    Will need to implement valid ranges instead of limiting to 1920*1080

--- src/App/Conf.py
+++ src/App/Conf.py
@@ -1,4 +1,5 @@
 import pickle
+from pyautogui import size
 
 
 class Conf:
@@ -16,6 +17,8 @@ class Conf:
     KEYS = {"PLACEHOLDER": "PLACEHOLDER"}
 
     DEFAULTS = {"PLACEHOLDER": "PLACEHOLDER"}
+    
+    SCREEN_SIZE: tuple = size() if (size()[0] > 1920 and size()[1] > 1080) else (1920, 1080)
 
     def __init__(self):
         # call this from somewhere else if there is no loadconf
