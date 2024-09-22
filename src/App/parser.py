ebc4315b428b511c643eb808379b103825695e5e
Icecreambobcat <icekingrex123@gmail.com>
2024-09-22 16:50:42 +1000

    Data object
    Attempts to lay out the basis for initialising all the level files into
    memory on game launch

--- src/App/parser.py
+++ src/App/parser.py
@@ -1,11 +1,13 @@
 from typing import Any, Union, Optional
+from States.Game import Level
 from lib import Lib
 from pathlib import Path
 import os
 
 
-def level_load(file) -> dict[str, object]:
+def level_load(file) -> dict[str, Level]:
+    out = dict()
     dir = Path(f"{Lib.GET_ROOT()}/Assets/Levels")
     for file in dir.iterdir():
-        pass
-    pass
+        out[file.name] = Level(file)
+    return out
