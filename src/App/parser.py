c43c7ac950b0efac90a009677bf2a09ae6dd8589
Icecreambobcat <icekingrex123@gmail.com>
2024-09-24 18:15:35 +1000

    Formatting

--- src/App/parser.py
+++ src/App/parser.py
@@ -1,16 +1,16 @@
 from __future__ import annotations
 from typing import Any
-from States.Game import Level
+from States.Game import Level_FILE
 from lib import Lib
 from pathlib import Path
 
 
-def level_load() -> dict[str, Level]:
+def level_load() -> dict[str, Level_FILE]:
     out = dict()
     dir = Path(f"{Lib.PROJECT_ROOT}/Assets/Levels")
     for file in dir.iterdir():
         for diff in file.iterdir():
             if diff.name.endswith(".osu"):
-                level = Level(diff)
+                level = Level_FILE(diff)
                 out[f"{level.meta["TitleUnicode"]} | {level.meta["Version"]}"] = level
     return out
